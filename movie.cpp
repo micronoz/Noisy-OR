471a2202365594b61f5ee9850f7c63fa51e95b3b
Nabi <nozberkm@ucsd.edu>
2018-05-21 21:14:42 -0700

    Check

--- movie.cpp
+++ movie.cpp
@@ -33,20 +33,30 @@ int main(int argc, char* agrv[]) {
     for (int j = 0; j < studentCount; j++) {
         vector<int> e;
         getline(movieRatings, s);
-        for (int i = 0; i < s.length(); i++) {
+        cout << s;
+        cout << endl << endl;
+        for (int i = 0; e.size() != movieCount; i++) {
+          cout << s.at(i);
             switch (s.at(i)) {
                 case '0': e.push_back(0);
                 case '1': e.push_back(1);
                 case '?': e.push_back(-1);
-                case '\t': ;
+                default: ;
             }
         }
         studentRatings[j] = e;
+        cout << endl << endl;
+        break;
     }
 
-    for (auto it = studentRatings.begin(); it != studentRatings.end(); it++) {
+    /*for (auto it = studentRatings.begin(); it != studentRatings.end(); it++) {
         for (auto it2 = it->begin(); it2 != it->end(); it2++)
             cout << *it2 << " ";
         cout << endl;
-    }
+    }*/
+
+    for (auto it = studentRatings[0].begin(); it != studentRatings[0].end(); it++)
+      cout << *it << '\t';
+    cout << endl;
+    cout << studentRatings[0].size() << endl;
 }
